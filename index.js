79fe32eec3e3330107284a12cdc07d11e1fb18be
KalpeshKs <kalpeshsharma701@gmail.com>
2023-01-26 16:47:46 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-document.getElementById("demo").innerHTML="Hello"
+btn.onclick = () => {
+
+    var person = document.getElementById("person").value;
+
+    var service = document.getElementById("service").value;
+    var bill = document.getElementById("bill").value;
+    var tip = bill * service / (100 * person);
+    document.getElementById('show').innerHTML = tip;
+    if (person == '' || service == '' || bill == '') {
+        return alert("Invalid input");
+    }
+    else if (person > 1) {
+        document.getElementById("each").style.display ="block";
+
+
+    }
+    else {
+         document.getElementById("each").style.display="none";
+    }
+
+}
